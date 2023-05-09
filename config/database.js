b6852e22defa11b977a65ac41e54a008e627a5ec
SolAndrade <solnicandrade@gmail.com>
2023-05-09 15:51:07 +0200

    [UPDATE] connection with DB in MySQL

---
+++ config/database.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2');
+
+// // Create a connection pool
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'ironhack',
+  database: 'Management'
+});
+
+// Export the pool for use in other modules
+module.exports = pool;
+
+
+//for postgre
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'Hickathon',
+//   password: 'user',
+//   port: 5432, // default PostgreSQL port
+// });
+
+
+
